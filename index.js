0f11462b39a31c3333ea45812d71785895a5e2ac
vkrdec <vkrdec@gmail.com>
2026-02-09 19:12:09 +0530

    added fix for cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import countries from './countries.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
